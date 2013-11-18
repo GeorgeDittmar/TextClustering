9fab25f384052a2ca1051800c381cccbe08d2ed5
georgedittmar <georgedittmar@gmail.comm>
2013-11-17 19:28:03 -0800

    began adding the calculate distance method for the KMeans class. We are using euclidian distance.

--- src/main/java/com/textclustering/kmeans/KMeans.java
+++ src/main/java/com/textclustering/kmeans/KMeans.java
@@ -1,5 +1,9 @@
 package com.textclustering.kmeans;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * Implements the kmeans algorithm.
  * User: george
@@ -14,7 +18,25 @@ public class KMeans {
     }
 
     // calculates the euclidian distance of a document to the cluster centers
-    public void calc_dist(){
+    public void calc_dist(Vector<Double> doc, List<Vector<Double>> centroids){
+
+        List<Double> distanceToCentroids = new LinkedList<Double>();
+        for(Vector<Double> centroid: centroids){
+            if(doc.size() != centroid.size()){
+                throw new IllegalArgumentException("Document and centroid vectors are not the same size. Some issue has happened" +
+                        "during creation of the vectors.");
+            }
+            double distance = 0.0;
+            for(int i=0; i<doc.size(); i++){
+
+               distance += Math.pow((doc.get(i)-centroid.get(i)),2);
+
+
+            }
+
+            distance = Math.sqrt(distance);
+            distanceToCentroids.add(distance);
+        }
 
     }
 }
