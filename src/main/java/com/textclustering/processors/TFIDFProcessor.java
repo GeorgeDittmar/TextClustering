19e28cddafd3a080fdf6c7ed37551c7ef62cfa44
georgedittmar <georgedittmar@gmail.comm>
2013-11-17 20:50:50 -0800

    began adding the calculate distance method for the KMeans class. We are using euclidian distance.

---
+++ src/main/java/com/textclustering/processors/TFIDFProcessor.java
@@ -0,0 +1,20 @@
+package com.textclustering.processors;
+
+import edu.stanford.nlp.ling.Document;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: george
+ * Date: 11/17/13
+ * Time: 7:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TFIDFProcessor {
+    public static void termvector(List<Document> documents){
+
+        // go through each document and create the master term vector
+
+    }
+}
