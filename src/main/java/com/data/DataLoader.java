86cbdf144ab9f73231d844b21a025c09c477968e
georgedittmar <georgedittmar@gmail.comm>
2013-11-17 20:58:08 -0800

    adding data loading classes that will build maps class labels to documents

---
+++ src/main/java/com/data/DataLoader.java
@@ -0,0 +1,13 @@
+package com.data;
+
+/**
+ * The DataLoader object's job is to take a directory and read in documents from sub folders, where the subfolders are
+ * used to denote document class. Example if you point the DataLoader to a folder named documents and there are 2 subfolders in it
+ * called fiction and non-fiction; these two sub folders will denote the number of clusters / classes we will try to learn.
+ * User: george
+ * Date: 11/17/13
+ * Time: 8:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataLoader {
+}
