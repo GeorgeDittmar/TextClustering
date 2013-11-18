b1bbedd1d17ce975cfa8d93f868f56fe669c98c1
georgedittmar <georgedittmar@gmail.comm>
2013-11-17 19:04:55 -0800

    initial commit of package structure and classes for kmeans and document processors.

---
+++ src/main/java/com/textclustering/kmeans/KMeans.java
@@ -0,0 +1,20 @@
+package com.textclustering.kmeans;
+
+/**
+ * Implements the kmeans algorithm.
+ * User: george
+ * Date: 11/17/13
+ * Time: 6:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KMeans {
+
+    public void KMeans(){
+
+    }
+
+    // calculates the euclidian distance of a document to the cluster centers
+    public void calc_dist(){
+
+    }
+}
---
+++ src/main/java/com/textclustering/processors/DocumentProcessor.java
@@ -0,0 +1,11 @@
+package com.textclustering.processors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: george
+ * Date: 11/17/13
+ * Time: 7:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DocumentProcessor {
+}
